5882b8c108e028772ed5a88c3efc3caf5c2cb2a7
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-27 15:08:09 -0400

    commit tree 865ad23966475d03df1a42964ef69cf693c7a12f

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+//hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === complement) return true
+    }
+
+  }
+  return false;
 }
 
 /* 
@@ -8,11 +18,30 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+*/ 
+//hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+//Iterate through each number in the array
+// for the current #, identify a complement that adds to the target (complement = target - currentNum)
+//Iterate through the rest of the array and check if any # is our complement
+//if so, return true.
+//If i reach the end of the array, return false
+
 
 /*
   Add written explanation of your solution here
 */
+
+// make a function called 'hasTargetSum' that checks if two #s from the array add up to some target. 
+
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+//returns true, since 3 and 7 add up to 10.
+
+hasTargetSum([1,2,5],4);
+//returns false 
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
